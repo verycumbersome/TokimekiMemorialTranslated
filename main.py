30a1a261cc844d1554668c8468a6cf8c3d899742
matthew jordan <matthewsages@gmail.com>
2021-03-19 20:36:19 -0700

    Add stdread

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import sys
+import time
 
+
+import py
 import tqdm
 import mmap
 
@@ -8,16 +11,24 @@ import utils
 
 import subprocess
 
+while(True):
+    capture = py.io.StdCaptureFD(out=False, in_=False)
+    out,err = capture.reset()
 
-def main():
-    f = os.open("/Users/matthewjordan/Library/Application Support/avocado/iso/Tokimeki Memorial - Forever with You (Japan)/Tokimeki Memorial - Forever with You (Japan) (Track 1).bin", os.O_RDWR)
-    mm = mmap.mmap(f, 0, prot=mmap.PROT_READ)
-
-    mm.seek(0x0008F5BC)
-    line = mm.readline()
-
-    addr = input()
+    print(out)
+    print(err)
+    time.sleep(1)
 
 
-if __name__=="__main__":
-    main()
+# def main():
+    # f = os.open("/Users/matthewjordan/Library/Application Support/avocado/iso/Tokimeki Memorial - Forever with You (Japan)/Tokimeki Memorial - Forever with You (Japan) (Track 1).bin", os.O_RDWR)
+    # mm = mmap.mmap(f, 0, prot=mmap.PROT_READ)
+
+    # mm.seek(0x0008F5BC)
+    # line = mm.readline()
+
+    # addr = input()
+
+
+# if __name__=="__main__":
+    # main()
