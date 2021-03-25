f919ab0c44dbbf7240080fc1817d0897a79ad6e7
matthew jordan <matthewsages@gmail.com>
2021-03-25 17:33:27 -0400

    Update

--- utils.py
+++ utils.py
@@ -7,27 +7,26 @@ from google_trans_new import google_translator
 translator = google_translator()
 
 
-# def read_hex(f_ptr, addr):
-    # for i in range(4):
-        # hexf = hexf.replace(" ", "")
+def read_hex(hexf):
+    for i in range(4):
+        hexf = hexf.replace(" ", "")
 
-        # out = []
-        # for k in [hexf[c:c+4] for c in range(i, len(hexf), 4)]:
-            # if ((k[0] == "8") or (k[0] == "9")):
-            # try:
-                # kana = bytes.fromhex(k).decode("shift_jisx0213", "ignore")
-                # out.append(kana)
+        out = []
+        for k in [hexf[c:c+4] for c in range(i, len(hexf), 4)]:
+            try:
+                kana = bytes.fromhex(k).decode("shift_jisx0213", "ignore")
+                out.append(kana)
 
-            # except:
-                # continue
+            except:
+                continue
 
-        # out = ''.join(out)
-        # out_trans = translator.translate(out, lang_tgt='en')
+        out = ''.join(out)
+        out_trans = translator.translate(out, lang_tgt='en')
 
-        # print(f"Translated: {out_trans}")
-        # print(f"Original: {out}\n")
+        print("Translated:", out_trans)
+        print("Original:", out)
 
-def read_hex(f_ptr, addr):
+def read_hex_file(f_ptr, addr):
     mm = mmap.mmap(f_ptr, 0, prot=mmap.PROT_READ)
     mm.seek(addr)
     line = mm.readline()
@@ -37,6 +36,15 @@ def read_hex(f_ptr, addr):
 
     return out
 
-# if __name__=="__main__":
-    # text = input()
-    # print(read_hex(text))
+# def split_hex():
+    # with open("/Users/matthewjordan/Library/Application Support/avocado/iso/Tokimeki Memorial - Forever with You (Japan)/Tokimeki Memorial - Forever with You (Japan) (Track 1).bin", "r") as f_in:
+        # for line in f_in:
+            # if sep in line:
+                # print(line)
+                # offsets.append(f_in.tell())
+
+        # file_ptr.read().split("\x00\x00\x08\xAE")
+
+if __name__=="__main__":
+    text = input()
+    print(read_hex(text))
