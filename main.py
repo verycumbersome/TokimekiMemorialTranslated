5b1ba63a5d129d2424e69069bee8ef31848ac6b0
matthew jordan <matthewsages@gmail.com>
2021-03-29 16:37:12 -0700

    Update translation table parsing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import translation
 import utils
 
 def main():
-    trans_table = translation.get_table("/Users/matthewjordan/Library/Application Support/avocado/iso/Tokimeki Memorial - Forever with You (Japan)/Tokimeki Memorial - Forever with You (Japan) (Track 1).bin")
+    trans_table = translation.create_table("/Users/matthewjordan/Library/Application Support/avocado/iso/Tokimeki Memorial - Forever with You (Japan)/Tokimeki Memorial - Forever with You (Japan) (Track 1).bin")
 
     p = subprocess.Popen(["./avocado"],
                          stdout=subprocess.PIPE,
@@ -18,17 +18,12 @@ def main():
             if (tmp != seq) and (seq != b""):
                 seq = utils.clean_seq(seq)
                 seq = utils.read_hex(seq, translate=False)
-                # print(type(seq))
                 h_key = hashlib.sha224(str(seq).encode("utf8")).hexdigest()
-                # print(seq)
-                # print(h_key)
-                # print(h_key in trans_table)
 
                 if h_key in trans_table:
                     print("TRANS TABLE", trans_table[h_key])
 
                 tmp = seq
-        print("\n\n")
 
 
 if __name__=="__main__":
--- translation.py
+++ translation.py
@@ -1,22 +1,27 @@
 import os
 import re
-import mmap
+import json
 
+import tqdm
+import mmap
 import hashlib
 
 import utils
 
-def get_table(filename):
+def create_table(filename):
     """Returns a translation table from a bin file"""
+
     # Open hex file
     f_ptr = os.open(filename, os.O_RDWR)
     mm = mmap.mmap(f_ptr, 0, prot=mmap.PROT_READ)
 
     # Initialize idx pointer values for all punctuation
+    pbar = tqdm.tqdm(total=len(mm))
     per_idx = mm.rfind(b"\x81\x42", 0, len(mm))  # Period
     qst_idx = mm.rfind(b"\x81\x48", 0, len(mm))  # Question mark
     end = curr = min(per_idx, qst_idx)
 
+    counter = 0
     table = {}
     while (curr > 1):
         per_idx = mm.rfind(b"\x81\x42", 0, end)  # Period
@@ -25,29 +30,36 @@ def get_table(filename):
 
         curr = max(min(per_idx, null_idx), min(qst_idx, null_idx))
 
-        # print("per", per_idx)
-        # print("qst", qst_idx)
-        # print("nul", null_idx)
-
         if ((end - curr) < 300):
             seq = mm[curr + 2:end + 2].replace(b"\x00", b"")  # Sentence
+
+            # Continue search if shift-jis not valid ascii
+            if not ((seq.hex()[:1] == "8") or (seq.hex()[:1] == "9")):
+                end = curr
+                continue
+
+            enc_seq = []
+            for l in re.split(b'(\x81\x42|\x81\x48)', seq):
+                enc_seq += [s.decode("shift-jis", "ignore") for s in l.split(b"\x20")]
+
+            enc_seq = "".join(enc_seq)
+            h_key = hashlib.sha224(str(enc_seq).encode("utf8")).hexdigest()
+
             seq = seq.replace(b"\x20", b"")
-            seq = str(seq.decode("shift-jis", "ignore"))
-            h_key = hashlib.sha224(str(seq).encode("utf8")).hexdigest()
+            seq = seq.replace(b"\x00", b"")
+            table[h_key] = utils.read_hex(str(seq.hex()), translate=False)
 
-            table[h_key] = str(seq)
-
-
-            # print("seq", str(seq))
-            # print(mm[curr + 2:end + 2].replace(b"\x00", b""))
-            # print()
-            # if "知ってるも何も、超有名人じゃないか。" in str(seq):
-                # print("FUCLKKKK")
-                # exit()
+        # if counter > 200:
+            # return table
 
         end = curr
+        counter += 1
+        pbar.update(counter)
 
+    pbar.close()
     return table
 
 if __name__=="__main__":
-    get_table("/Users/matthewjordan/Library/Application Support/avocado/iso/Tokimeki Memorial - Forever with You (Japan)/Tokimeki Memorial - Forever with You (Japan) (Track 1).bin")
+    with open("translation_table.json", "w") as json_file:
+        translation_table = create_table("/Users/matthewjordan/Library/Application Support/avocado/iso/Tokimeki Memorial - Forever with You (Japan)/Tokimeki Memorial - Forever with You (Japan) (Track 1).bin")
+        json.dump(translation_table, json_file, indent=4)
