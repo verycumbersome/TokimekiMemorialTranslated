4fcd75594395f9fd0629e67e26be0a7971dcc837
matthew jordan <matthewsages@gmail.com>
2021-03-29 14:49:44 -0700

    Get trans table working

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import hashlib
 import subprocess
 
 import translation
 import utils
 
 def main():
-    # trans_table = translation.get_table("/Users/matthewjordan/Library/Application Support/avocado/iso/Tokimeki Memorial - Forever with You (Japan)/Tokimeki Memorial - Forever with You (Japan) (Track 1).bin")
+    trans_table = translation.get_table("/Users/matthewjordan/Library/Application Support/avocado/iso/Tokimeki Memorial - Forever with You (Japan)/Tokimeki Memorial - Forever with You (Japan) (Track 1).bin")
 
     p = subprocess.Popen(["./avocado"],
                          stdout=subprocess.PIPE,
@@ -12,12 +13,21 @@ def main():
 
     tmp = None
     while True:
-        lines = utils.clean_seq(p.stdout.readline().strip()).split(" 0 ")
-        for l in lines:
-            if (len(l) > 20):
-                if (tmp != l) and (l != b""):
-                    utils.read_hex(l, translate=False)
-                    tmp = l
+        seq = p.stdout.readline().strip().split(b" 0 ")[0]
+        if (len(seq) > 20):
+            if (tmp != seq) and (seq != b""):
+                seq = utils.clean_seq(seq)
+                seq = utils.read_hex(seq, translate=False)
+                # print(type(seq))
+                h_key = hashlib.sha224(str(seq).encode("utf8")).hexdigest()
+                # print(seq)
+                # print(h_key)
+                # print(h_key in trans_table)
+
+                if h_key in trans_table:
+                    print("TRANS TABLE", trans_table[h_key])
+
+                tmp = seq
         print("\n\n")
 
 
