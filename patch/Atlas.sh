5fd2dbd11bbeac6199350c3e519439eb6a0929d4
matthew jordan <matthewsages@gmail.com>
2021-04-09 06:41:00 -0700

    Update

--- patch/Atlas.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cp "/Users/matthewjordan/Library/Application Support/avocado/iso/test version/Test Version.bin" Atlas.bin 2> /dev/null;
-if [ "$?" != "0" ]; then
-	echo "ROM file \"TokiMeki Memorial.bin\" not found; creating an empty file instead.";
-	cat /dev/null > Atlas.bin;
-fi
-
-perl abcde/abcde.pl -cm abcde::Atlas Atlas.bin Atlas.txt
